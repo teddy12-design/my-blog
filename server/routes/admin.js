3f7c2c3d86cb4c727481f34254e43653f75cce80
Your Name <youremail@example.com>
2025-12-14 02:33:08 -0800

    Force update admin.js to fix multer reference

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const Post = require('../models/Post');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
 
 const adminLayout = '../views/layouts/admin';
 const jwtSecret = process.env.JWT_SECRET;
